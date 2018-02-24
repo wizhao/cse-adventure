39cde6b77eaed512df4f60f8270fe0a03c3c4229
Bill Zhao <31193099+wizhao@users.noreply.github.com>
2018-02-24 12:38:02 -0500

    Allow better canvas update inputs
    now u input a 2-tuple (str picname, str caption)

--- gui.py
+++ gui.py
@@ -13,7 +13,7 @@ class Application(Frame):
         '''Picture Section'''
         self.pic_container = Frame(height=300,width=400,padx=5,pady=5)
         self.pic_disp = Label(self.pic_container,bd=2,relief=SUNKEN,compound=TOP,textvariable=self.caption,image=ImageTk.PhotoImage(Image.open(os.getcwd() + "\\assets\\title.png")))
-        self.update_canvas("title.png")
+        self.update_canvas(self.currentLocation)
         self.pic_disp.pack()
         self.pic_container.grid(row=1,column=1,sticky=NE)
 
@@ -47,7 +47,7 @@ class Application(Frame):
         self.consoleScrollbar = Scrollbar(self.outputContainer)
         self.consoleScrollbar.pack(side=RIGHT, fill=Y)
 
-        self.output = Text(self.outputContainer,bd=0,width=50,height=17,yscrollcommand=self.consoleScrollbar.set,state="disabled")
+        self.output = Text(self.outputContainer,bd=0,width=50,height=18,yscrollcommand=self.consoleScrollbar.set,state="disabled")
         self.consoleScrollbar.config(command=self.output.yview)
         self.output.tag_config("g",foreground="green")
         self.output.tag_config("r",foreground="red")
@@ -55,7 +55,7 @@ class Application(Frame):
         self.output.pack()
         self.output.bind('<<Modified>>', self.callback)
 
-        #test inputs
+        '''test inputs
         self.update_console("Welcome to Osu")
         self.update_console("Welcome to Osu x2")
         self.update_console("Welcome to Osu x3 ")
@@ -68,7 +68,7 @@ class Application(Frame):
                 self.update_console("Crissmass",tag="r")
         self.update_console("What the frick did you just flipping say abotu me, you little witch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Qauaeda, and I have over 300 confirmed kills. I am trianed in orilla woarfare and I'm the to pscniper in the entire US frarmed forces. Ypu are nothing to me but jsut anot er target. I will wipe you the frick out with prescision the lies of which ash never been see nbefore on thies Earth, mark my frickning words.")
         self.update_console("anogiabgabrgergh928293r9238749y9rhgdlgdqwertyuiopasdfghjklzxcvbnm",tag="g")
-        #end test inputs
+        #end test inputs'''
 
         self.outputFrame.grid(row=1,column=0,sticky=N+S+E+W)
 
@@ -101,8 +101,8 @@ class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.caption = StringVar()
-        self.currentTitle = "title.png"
-        self.caption.set("Bottom Text")
+        self.currentLocation = ("title.png","Bottom Text")
+        self.caption.set(self.currentLocation[1])
         self.grid()
         self.pack_propagate(0)
         self.createWidgets()
@@ -124,17 +124,17 @@ class Application(Frame):
         cancel.pack(side=LEFT,padx=5)
 
     def onselect(self,evt):
-        self.update_canvas("itemGeneric.png")
+        self.update_canvas(("itemGeneric.png","A caption."))
         w = evt.widget
         index = int(w.curselection()[0])
         value = w.get(index)
-        self.caption.set(value)
+        #self.caption.set(value)
         self.update_console('You selected the %s' % (value))
         self.dropgun.pack_forget()
         self.dropgun.pack()
 
     def deselect_item(self,evt):
-        self.update_canvas("title.png")
+        self.update_canvas(self.currentLocation)
         self.dropgun.pack_forget()
 
     def drop_item(self):
@@ -154,13 +154,9 @@ class Application(Frame):
         self.output.insert(END,prefix + message,(tag,"wrap"))
         self.output.config(state="disabled")
 
-    def update_canvas(self, picname,caption="\n\n"):
-        picname = os.getcwd() + "\\assets\\" + picname
+    def update_canvas(self, picinfo):
+        picname = os.getcwd() + "\\assets\\" + picinfo[0]
         photo = ImageTk.PhotoImage(Image.open(picname))
         self.pic_disp.config(image=photo)
         self.pic_disp.currentImage = photo
-        self.caption.set(caption)
-
-
-
-
+        self.caption.set(picinfo[1])
