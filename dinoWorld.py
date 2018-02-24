1f126db828ce223d0aa7e4573fd1289c65f434f7
zcolucci <zacolucci@ctemc.org>
2018-02-24 17:55:05 -0500

    Capitalization
    just a little bit of capitalization

--- dinoWorld.py
+++ dinoWorld.py
@@ -24,11 +24,11 @@ def run(a):
     app.set_b(b)
     
 def start():
-    app.update_console('To your left you see a towering stone temple, in the distance, you see a sleeping T-Rex, and to your right, you see an abandoned campsite. Which way do you turn?')
+    app.update_console('To your left you see a towering stone temple, in the distance, you see a sleeping T-Rex, and to your right, you see an abandoned campsite. Which way do you turn?\n')
     app.update_buttons([('Jungle Temple', op1), ('Sleeping T-Rex', op2), ('Abandoned Campsite', op3), ('Return to Hub', lambda: hub.run(app))])
         
 def op1():
-    app.update_console('You walk into the Jungle Temple and see eight switches on the wall in front of you. To the right of that wall is a staircase, and on the left side of the room is a dilapidated chest. Where do you choose to go?')
+    app.update_console('You walk into the Jungle Temple and see eight switches on the wall in front of you. To the right of that wall is a staircase, and on the left side of the room is a dilapidated chest. Where do you choose to go?\n')
     app.update_buttons([('Switches', op1_1), ('Chest', op1_2), ('Staircase', op1_3), ('Back', start)])
         
 def op1_1():
@@ -42,7 +42,7 @@ def op1_1():
         print '\n'
         if (ans == '01000101'):
             print ('Correct combination! A part of the wall retracts and a bone shiv is dispensed!\n')
-            b.add("bone shiv")
+            b.add("Bone Shiv")
             switches = True
         else:
             print ('That combination does nothing.\n')
@@ -53,21 +53,22 @@ def op1_1():
     
 def op1_2():
     global chest
+    app.update_console('chest: ' + str(chest))
     if (chest==False):
         app.update_console( 'The worn-down chest opens with a creaking sound, revealing: ')
         loot = random.randint(0,3)
         if (loot == 0):
             app.update_console( 'Steamed Hams and Macaroni!\n', tag="g")
-            b.add("steamed hams and macaroni")
+            b.add("Steamed Hams and Macaroni")
         if (loot == 1):
             app.update_console( 'M&Ms!\n', tag="g")
             b.add("M&Ms")
         if (loot == 2):
             app.update_console( 'Burger King Foot Lettuce!\n', tag="g")
-            b.add("lettuce")
+            b.add("Lettuce")
         if (loot == 3):
             app.update_console( 'Gravy!\n', tag="g")
-            b.add("gravy")
+            b.add("Gravy")
         chest = True  
     else:
         app.update_console('This chest has already been opened.\n')
@@ -78,27 +79,30 @@ def op1_3():
     global lives
     global darkRoom
     if (darkRoom == False):
-        app.update_console( 'You descend the stairs and find a very dark room. You believe that there could be something useful in this room. You decide to search for a lightswitch. Do you...')
+        app.update_console( 'You descend the stairs and find a very dark room. You believe that there could be something useful in this room. You decide to search for a lightswitch. Do you...\n')
         app.update_buttons([('Walk', op1_3_1), ('Crawl', op1_3_2), ('Hug the wall', op1_3_3), ('Back', op1)])
     else:
         app.update_console( 'You decide there\'s nothing left for you downstairs\n')
+        op1()
     
 
 def op1_3_1():
     global darkRoom
+    global lives
     app.update_console( 'You try walking but unfortunately hit a few traps on the floor to the lightswitch. (-1 life)', tag="r")
-    #lives-=1
+    app.add_life(-1)
     app.update_console( 'You find the switch and see a futuristic-looking key on the ground. You pick up the key and put it in your backpack.\n','g')
-    b.add('key')
+    b.add('Key')
     darkRoom = True
     op1()
     
 def op1_3_2():
     global darkRoom
+    global lives
     app.update_console( 'You try crawling but unfortunately hit a few traps on the floor to the lightswtich. (-1 life)', tag="r")
-    #lives-=1
+    app.add_life(-1)
     app.update_console( 'You find the switch and see a futuristic-looking key on the ground. You pick up the key and put it in your backpack.\n', tag='g')
-    b.add('key')
+    b.add('Key')
     darkRoom = True
     op1()
     
@@ -106,7 +110,7 @@ def op1_3_3():
     global darkRoom
     app.update_console( 'You successfully make it to the other side of the room without getting hurt', tag="g")
     app.update_console( 'You find the switch and see a futuristic-looking key on the ground. You pick up the key and put it in your backpack.\n', tag='g')
-    b.add('key')
+    b.add('Key')
     darkRoom = True
     op1()
         
@@ -116,25 +120,25 @@ def op2():
     if (tRex == False):
         app.update_console( 'You decide it would be a good idea to kill this sleeping dinosaur to get its skull.')
         if (b.get_items('weapon') != []):
-            app.update_console( 'You take out your ' + random.choice(b.get_items('weapon')) + ' and attack the dinosaur, killing it. You recieved its skull as a reward.', tag='g')
-            b.add('skull')
+            app.update_console( 'You take out your ' + random.choice(b.get_items('weapon')) + ' and attack the dinosaur, killing it. You recieved its skull as a reward.\n', tag='g')
+            b.add('Skull')
             tRex = True
         else:
-            app.update_console( 'However, you don\'t want to attack it without some sort of weapon.')
+            app.update_console( 'However, you don\'t want to attack it without some sort of weapon.\n')
     start()
     
 def op3():
     global necklace
     if (necklace == False):
         app.update_console( 'At the abandoned campsite, you see a shaggy man with a wispy beard. The insane-looking man tells you that he has a power crystal to give you if you get him a **Seashell Necklace**. You decide to trust the man\'s word.')
-    app.update_console( 'Throughout the rest of the campsite, you see two tents and a campfire. What do you do?')
-    if (b.has('seashell necklace')):
-        app.update_buttons([('Search first tent', op3_1), ('Search second tent', op3_2), ('Search campfire', op3_3), ('Back', op3), ('Give necklace', op3_4)])
+    app.update_console( 'Throughout the rest of the campsite, you see two tents and a campfire. What do you do?\n')
+    if (b.has('Seashell Necklace')):
+        app.update_buttons([('Search first tent', op3_1), ('Search second tent', op3_2), ('Search campfire', op3_3), ('Back', start), ('Give necklace', op3_4)])
     else:
-        app.update_buttons([('Search first tent', op3_1), ('Search second tent', op3_2), ('Search campfire', op3_3), ('Back', op3), ('Give necklace', op3_4)])
+        app.update_buttons([('Search first tent', op3_1), ('Search second tent', op3_2), ('Search campfire', op3_3), ('Back', start)])
 
 def op3_1():
-    app.update_console( 'You find nothing in the first tent.')
+    app.update_console( 'You find nothing in the first tent.\n')
     op3()
 def op3_2():
     global lives
@@ -146,28 +150,28 @@ def op3_2():
         else:
             chance = random.randint(0,1)
             if (chance == 0):
-                app.update_console( 'The raptor hits you with a devastating strike. (-1 life)', tag='r')
-                #lives-=1
+                app.update_console( 'The raptor hits you with a devastating strike. (-1 life)\n', tag='r')
+                app.add_life(-1)
             if (chance == 1):
-                app.update_console( 'Luckily, you are able to kill the raptor with your bare fists without a scratch.')
+                app.update_console( 'Luckily, you are able to kill the raptor with your bare fists without a scratch.\n')
         raptor = True
     else:
-        app.update_console( 'The tent is now empty after the raptor left')
+        app.update_console( 'The tent is now empty after the raptor left\n')
     op3()
     
 def op3_3():
     global campfire
     if (campfire == False):
-        app.update_console( 'You find coal in the campfire')
-        b.add('coal')
+        app.update_console( 'You find coal in the campfire\n')
+        b.add('Coal')
         campfire = True
     else:
-        app.update_console( 'You find nothing else in the campfire.')
+        app.update_console( 'You find nothing else in the campfire.\n')
     op3()
 def op3_4():
     global necklace
-    app.update_console( 'You hand the seashell necklace over to the man, and, as he promised, he gifts you a green power crystal')
-    b.add('green crystal')
-    b.remove('seashell necklace')
+    app.update_console( 'You hand the seashell necklace over to the man, and, as he promised, he gifts you a green power crystal.\n', tag='g')
+    b.add('Green Crystal')
+    b.remove('Seashell Necklace')
     necklace = True
     op3()
--- postApocWorld.py
+++ postApocWorld.py
@@ -11,19 +11,18 @@ def run(a):
     global app
     global b
     app = a
-    app.update_console('You enter this fallow, dystopian world gasping for your breath as the dust invades your nostrils.')
+    app.update_console('You enter this fallow, dystopian world gasping for your breath as the dust invades your nostrils.\n')
     b = app.get_b()
     start()
     app.set_b(b)
 
 #starting point
 def start():
-    app.update_console('\nWithin your field of view, you can see a massive glass dome, a group of off-road vehicles, and a gang of survivors. Which area do you visit first?')
+    app.update_console('Within your field of view, you can see a massive glass dome, a group of off-road vehicles, and a gang of survivors. Which area do you visit first?\n')
     app.update_buttons([('Thunderdome', op1), ('Off-road Vehicles', op2), ('Gang of Survivors', op3), ('Return to Hub', lambda: hub.run(app))])
 
 #option 1
 def op1():
-    global lives
     challenger = random.choice(['jeff', 'Adithya the Almighty', 'Daniel the Dauntless', 'Con Hndenberg', 'Sidhu the Sadistic', 'Glasser the Graceful', 'Nelson the Nightmare'])
     app.update_console( 'A man wearing a bucket as a helmet walks up to you and shouts: "WELCOME TO THE THUNDERDOME! Today you will face off against the next challenger: ' + challenger + '!"\n')
     app.update_console( 'To fight, you will roll two six-sided dice to determine if you lose. A loss will make you lose a life, but a win will let you gain another. Winning also can get you other sweet rewards.')
@@ -46,33 +45,33 @@ def op1_1(minRoll, challenger):
             app.update_console( 'Despite ' + challenger + '\'s brute force and dexterity, you are able to knock them down with your ' + random.choice(b.get_items('weapon')) + ' with grace. (+1 life)', tag='g')
             loot = random.randint(0, 9)
             if (loot < 5):
-                if (not b.has('human meat')):
+                if (not b.has('Human Meat')):
                     app.update_console( 'For your victory, you recieve some human meat.', tag='g')
-                    b.add('human meat')
+                    b.add('Human Meat')
             if (loot == 6):
-                if (not b.has('crossbow')):
+                if (not b.has('Crossbow')):
                     app.update_console( 'For your victory, you recieve a crossbow!', tag='g')
-                    b.add('crossbow')
-            #lives+=1
+                    b.add('Crossbow')
+            app.add_life(1)
         else:
             app.update_console( challenger + ' runs at you full speed and clobbers you in the skull with a massive blow. (-1 life)', tag='r')
-            #lives-=1
+            app.add_life(-1)
     if (minRoll == 9):
         if (total > minRoll):
             app.update_console( 'To the shock of the audience, you pummel down ' + challenger + ' with your bare fists. (+1 life)', tag='g')
             loot = random.randint(0, 9)
             if (loot < 5):
-                if (not b.has('human meat')):
+                if (not b.has('Human Meat')):
                     app.update_console( 'For your victory, you recieve some human meat.', tag='g')
-                    b.add('human meat')
+                    b.add('Human Meat')
             if (loot == 6):
-                if (not b.has('crossbow')):
+                if (not b.has('Crossbow')):
                     app.update_console( 'For your victory, you recieve a crossbow!', tag='g')
-                    b.add('crossbow')
-            #lives+=1
+                    b.add('Crossbow')
+            app.add_life(1)
         else:
             app.update_console( challenger + ' takes advantage of your lack of protection and knocks you out in a single punch. (-1 life)', tag='r')
-            #lives-=1
+            app.add_life(-1)
     app.update_console( 'You can always try your hand at the THUNDERDOME again.\n')
     op1()
         
