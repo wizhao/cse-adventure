250c06e8d7a044001ce8c12c2815084cb67be9bf
adithyaparams <adithyaparams@gmail.com>
2018-02-25 22:35:53 -0500

    Add player comments

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 class Player(object):
+    #initialize object in Player class
     def __init__(self):
         self.lives = 5
         self.chestOpened = False
-    
+
+    #reset number of lives
     def set_lives(self, lives):
         self.lives = lives
-    
+
+    #retrieve number of lives
     def get_lives(self):
         return self.lives
-        
-    
