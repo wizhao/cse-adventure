666fa83d2639d2188796643d00c67abd5dc32286
adithyaparams <adithyaparams@gmail.com>
2018-02-25 22:40:55 -0500

    Add some more comments

--- item.py
+++ item.py
@@ -1,10 +1,10 @@
 class Item(object):
+#Item class
+
     def __init__(self, name=None, caption="Bottom Text", ilk="tool", picture="itemGeneric.png",droppable=True,reason=None):
+        #initialize object in Item class
         self.name = name
         self.caption = caption
         self.ilk = ilk
         self.picture = picture
         self.droppable = droppable
-
-# pencil = Item('pencil', 'used to write stuff', 'utensil', 'lead-based')
-# print pencil.description
