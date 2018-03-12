325ad967c7602fc2784449e118c8fff0e991e627
zcolucci <zacolucci@ctemc.org>
2018-03-12 13:34:57 -0400

    hopefully fixed future issue
    change to futureWorld

--- hub.py
+++ hub.py
@@ -4,7 +4,7 @@ import dinoWorld
 import atlantis
 import warWorld
 import iceWorld
-import future
+import futureWorld
 import postApocWorld
 
 def run(app):
@@ -13,7 +13,7 @@ def run(app):
         app.win()
     app.change_location(("misc\\hub.png","A quiet, gateway world, open to various dimensions."))
     app.update_console('You arrive at the hub. Large arrays of portals swirl lazily.\n\nChoose a world to visit:\n')
-    app.update_buttons([('Atlantis', lambda: atlantis.run(app)), ('Future', lambda: future.run(app)),
+    app.update_buttons([('Atlantis', lambda: atlantis.run(app)), ('Future', lambda: futureWorld.run(app)),
                         ("Post-Apocalyptic World", lambda: postApocWorld.run(app)),
                         ("Dino World", lambda: dinoWorld.run(app)), ('War World', lambda: warWorld.run(app)),
                         ('Ice World', lambda: iceWorld.run(app)), ('Storage', app.pull_out)])
